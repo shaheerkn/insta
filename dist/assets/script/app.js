71d9493ac6f0c26a4aca8f675cc1dea5ed50fcc6
Muhammad Shaheer <shaheer@uptek.com>
2022-12-30 00:43:26 +0500

    intial commit

---
+++ dist/assets/script/app.js
@@ -0,0 +1,90 @@
+const navSearch = document.querySelector(".navSearch");
+const faq = document.querySelectorAll(".faqLi");
+const faqMessage = document.querySelectorAll(".faqs-message");
+const headerBtn = document.getElementById("headerBtn");
+const headerMbl = document.querySelector(".headerBtn");
+
+if (navSearch != null) {
+  navSearch.addEventListener("click", () => {
+    document.querySelector(".navSearchWrapper > input").style.opacity = 1;
+  });
+}
+
+if (faq) {
+  for (let i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", () => {
+      faqMessage.forEach((el, index) => {
+        if (i == index) {
+          el.classList.toggle("hidden");
+        }
+      });
+    });
+  }
+}
+
+if (headerBtn != null) {
+  headerBtn.addEventListener("click", function () {
+    document.getElementById("dropdown").classList.toggle("hidden");
+  });
+}
+
+if (headerMbl != null) {
+  headerMbl.addEventListener("click", function () {
+    document.querySelector(".mbl-dropdown").classList.toggle("hidden");
+  });
+}
+
+let mySwiper = document.querySelector(".mySwiper");
+
+if (mySwiper) {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 12,
+      },
+    },
+  });
+}
+
+let dropdownLinks = document.querySelectorAll("#dropdown li a");
+let searchInput = document.querySelector("#search");
+let mobileMenu = document.querySelector(".mobile-menu");
+let navbar = document.querySelector(".mobile-navbar");
+let closeNavbar = document.querySelector(".close-navbar");
+
+if (dropdownLinks && searchInput) {
+  dropdownLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log(link.getAttribute("href"));
+      searchInput.value = link.getAttribute("href");
+    });
+  });
+}
+
+mobileMenu.addEventListener("click", () => {
+  navbar.classList.remove("-left-60");
+  navbar.classList.add("left-0");
+});
+
+closeNavbar.addEventListener("click", () => {
+  navbar.classList.add("-left-60");
+  navbar.classList.remove("left-0");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "4xl": "1540px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
